897d0cbb863d4f5543a908e89d8158922a19b3fe
Nikita <flesakov.nik09@gmail.com>
2021-09-26 02:21:55 +0500

    add dashboard

--- education/views.py
+++ education/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "wrapper.html")
+
+
+def dashboard(request):
+    return render(request, "dashboard.html")
