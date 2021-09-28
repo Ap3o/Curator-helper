9630e34554d8f0a5e90a7b271753f39947bf40c7
Nikita <flesakov.nik09@gmail.com>
2021-09-29 01:03:11 +0500

    При нажатии на редактирование берется имя редактируемого студента. #TODO

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^students/$', views.index, name="get_students")
+    url('^students/$', views.index, name="get_students"),
+    url('^students/s/$', views.search_for_one_student, name="search_for_one_student")
 ]
--- api/views.py
+++ api/views.py
@@ -6,3 +6,10 @@ def index(request):
     students = Student.objects.filter(full_name__contains=request.GET.get('term', ''))
     students_array = [{'id': student.id, 'text': student.full_name} for student in students]
     return JsonResponse({'results': students_array})
+
+
+def search_for_one_student(request):
+    student_id = request.GET.get('term', '')
+    student_name = Student.objects.get(id=request.GET.get('term', '')).full_name
+    student = [{'id': student_id, 'text': student_name}]
+    return JsonResponse({'results': student})
