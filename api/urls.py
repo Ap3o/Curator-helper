79a3c2ae0b32b2ce4e557a3dfefecb0af19313b1
Student <flesakov.nik09@gmail.com>
2021-09-29 13:31:46 +0500

    Автозаполнение из AcademicPerfomance

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     url('^students/$', views.index, name="get_students"),
-    url('^students/s/$', views.search_for_one_student, name="search_for_one_student")
+    url('^students/s/$', views.search_for_one_student, name="search_for_one_student"),
+    url('^subjects/$', views.get_subjects, name="get_subjects_link"),
+    url('^teachers/$', views.get_teachers, name="get_teachers_link"),
+    url('^acperf/$', views.get_academicperfomance, name="get_academicperfomance_link"),
+    # url('^subjects/$', views.get_subjects, name="get_subjects_link"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from education.models import Student
+from education.models import Student, Subject, Teacher, AcademicPerformance
 
 
 def index(request):
@@ -13,3 +13,28 @@ def search_for_one_student(request):
     student_name = Student.objects.get(id=request.GET.get('term', '')).full_name
     student = [{'id': student_id, 'text': student_name}]
     return JsonResponse({'results': student})
+
+
+def get_subjects(request):
+    subjects = Subject.objects.filter(name__contains=request.GET.get('term', ''))
+    subjects_array = [{'id': subject.id, 'text': subject.name} for subject in subjects]
+    return JsonResponse({'results': subjects_array})
+
+
+def get_teachers(request):
+    teachers = Teacher.objects.filter(full_name__contains=request.GET.get('term', ''))
+    teachers_array = [{'id': teacher.id, 'text': teacher.full_name} for teacher in teachers]
+    return JsonResponse({'results': teachers_array})
+
+
+def get_academicperfomance(request):
+    academperfomance = AcademicPerformance.objects.get(id=request.GET.get('term', ''))
+    return JsonResponse({'student_id': academperfomance.student.id,
+                         'student_name': academperfomance.student.full_name,
+                         'teacher_id': academperfomance.teacher.id,
+                         'teacher_name': academperfomance.teacher.full_name,
+                         'subject_id': academperfomance.subject.id,
+                         'subject_name': academperfomance.subject.name,
+                         'type': academperfomance.type_of_perfomance,
+                         'mark': academperfomance.mark
+                         })
