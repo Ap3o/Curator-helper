69d43d6b0669bc97b764b7f6b44838620157fb1d
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-17 13:40:47 +0500

    new app curator_work

---
+++ curator_work/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CuratorWorkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'curator_work'
---
+++ curator_work/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+
+# Отчёт
+class Report(models.Model):
+    content = models.CharField(max_length=200)
+    start_at = models.DateTimeField(verbose_name='Период от')
+    end_at = models.DateTimeField(verbose_name='Период до')
+
+    class Meta:
+        verbose_name_plural = "Отчёты"
+
+
+# Родительские собрания
+class ParentTeacherMeeting(models.Model):
+    content = models.CharField(max_length=200, verbose_name='Содержание')
+    date = models.DateField(verbose_name='Дата')
+
+    class Meta:
+        verbose_name_plural = "Родительские собрания"
+
+
+# Мероприятия воспитательной работы
+class EducationalActivities(models.Model):
+    activity = models.CharField(max_length=200, verbose_name='Мероприятие')
+    date = models.DateField(verbose_name='Дата')
+    note = models.CharField(max_length=1000, verbose_name='Примечание')
+
+    class Meta:
+        verbose_name_plural = "Мероприятия воспитальной работы"
