4910f264d92a1450c3142a88c894f5a14429d102
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-21 01:31:54 +0500

    admin register models

--- curator_work/admin.py
+++ curator_work/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Report)
+class AdminParent(admin.ModelAdmin):
+    list_display = ('content', 'start_at', 'end_at',)
+
+
+@admin.register(models.EducationalActivities)
+class AdminParent(admin.ModelAdmin):
+    list_display = ('activity', 'date', 'note',)
+
+
+@admin.register(models.ParentTeacherMeeting)
+class AdminParent(admin.ModelAdmin):
+    list_display = ('content', 'date', )
--- homeroom/admin.py
+++ homeroom/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.ClassPeriod)
+class AdminParent(admin.ModelAdmin):
+    list_display = ('topic', 'month',)
+
+
+@admin.register(models.SpentClassPeriod)
+class AdminParent(admin.ModelAdmin):
+    list_display = ('class_period', 'content',)
