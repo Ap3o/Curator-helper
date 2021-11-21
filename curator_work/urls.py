e808db02b9271f974c8a02805de260dceb675e28
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-21 19:51:56 +0500

    curator_work code review

--- curator_work/urls.py
+++ curator_work/urls.py
@@ -4,27 +4,10 @@ from . import views
 urlpatterns = [
     # report
     url('^report/$', views.report, name="report"),
-    url('^report/get-modal/$', views.report_modal, name="modal_report"),
-    url('^report/edit/(?P<pk>\d+)/$', views.report_edit, name="edit_report"),
-    url('^report/delete/$', views.report_delete,
-        name="delete_report"),
-    url('^report/create/$', views.report_create, name="create_report"),
 
     # EducationalActivities
     url('^educationalactivities/$', views.educationalactivities, name="educationalactivities"),
-    url('^educationalactivities/get-modal/$', views.educationalactivities_modal, name="modal_educationalactivities"),
-    url('^educationalactivities/edit/(?P<pk>\d+)/$', views.educationalactivities_edit,
-        name="edit_educationalactivities"),
-    url('^educationalactivities/delete/$', views.educationalactivities_delete,
-        name="delete_educationalactivities"),
-    url('^educationalactivities/create/$', views.educationalactivities_create, name="create_educationalactivities"),
 
     # ParentTeacherMeeting
     url('^parentteachermeeting/$', views.parentteachermeeting, name="parentteachermeeting"),
-    url('^parentteachermeeting/get-modal/$', views.parentteachermeeting_modal, name="modal_parentteachermeeting"),
-    url('^parentteachermeeting/edit/(?P<pk>\d+)/$', views.parentteachermeeting_edit,
-        name="edit_parentteachermeeting"),
-    url('^parentteachermeeting/delete/$', views.parentteachermeeting_delete,
-        name="delete_parentteachermeeting"),
-    url('^parentteachermeeting/create/$', views.parentteachermeeting_create, name="create_parentteachermeeting"),
 ]
--- curator_work/views.py
+++ curator_work/views.py
@@ -6,161 +6,104 @@ from . import models
 from . import forms
 
 
+def process_the_request(request, model, form, table_link, save_model_func, get_initial_data_func):
+    if request.method == "GET":
+        objects = model.objects.all()
+        return render(request, table_link, {"content": objects})
+
+    if request.is_ajax():
+        if request.method == "POST":
+
+            if request.POST["type"] == "delete":
+                model.objects.get(id=request.POST["id"]).delete()
+                return JsonResponse({"result": True, "text": "Запись удалена!"})
+
+            if request.POST["type"] == "get_modal":
+                # Если id был передан, значит запись надо отредактировать. Если не передан - создать новую.
+                if 'id' in request.POST:
+                    _id = request.POST['id']
+                    model_to_edit = model.objects.get(id=_id)
+                    initial_data = get_initial_data_func(model_to_edit)
+                    form = form(initial=initial_data)
+                else:
+                    _id = None
+                    form = form()
+                return render(request, "modals/modal.html", {"form": form, "id": _id})
+
+            if request.POST["type"] == "save":
+                if request.POST['id'] == 'None':
+                    save_model = model()
+                    text = "Запись добавлена!"
+                else:
+                    save_model = model.objects.get(id=request.POST['id'])
+                    text = "Запись отредактирована!"
+
+                save_model_func(save_model)
+                return JsonResponse({"result": True, "text": text})
+
+
+@require_http_methods(["GET", "POST"])
 def report(request):
-    objects = models.Report.objects.all()
-    return render(request, "tables/curator_work/report.html", {"content": objects})
+    def get_initial_data(_model):
+        initial_data = {
+            "content": _model.content,
+            "start_at": _model.start_at,
+            "end_at": _model.end_at,
+        }
+        return initial_data
+
+    def save_model(_model):
+        # Поля для изменения в каждой форме.
+        _model.content = request.POST.get('content')
+        _model.start_at = request.POST.get('start_at')
+        _model.end_at = request.POST.get('end_at')
+        _model.save()
+
+    model = models.Report
+    form = forms.ReportForm
+    table_link = "tables/curator_work/report.html"
+    return process_the_request(request, model, form, table_link, save_model, get_initial_data)
 
 
-def report_modal(request):
-    if request.method == "GET":
-        initial_id = request.GET.get("initial_id", '')
-        if initial_id != '':
-            # Если это запись для редактирования
-            initial_model = models.Report.objects.get(id=initial_id)
-
-            initial_data = {
-                "content": initial_model.content,
-                "start_at": initial_model.start_at,
-                "end_at": initial_model.end_at,
-            }
-            form = forms.ReportForm(initial=initial_data)
-            return render(request, "modals/curator_work/modalReport.html", {"form": form, 'initial_id': initial_id})
-        else:
-            form = forms.ReportForm()
-            return render(request, "modals/curator_work/modalReport.html", {"form": form, 'initial_id': ''})
-
-
-@require_http_methods(["GET"])
-def report_delete(request):
-    models.Report.objects.get(id=request.GET.get("id", '')).delete()
-    return JsonResponse({"result": True, "text": "Запись удалена!"})
-
-
-@require_http_methods(["POST"])
-def report_edit(request, pk):
-    model = models.Report.objects.get(id=pk)
-
-    model.content = request.POST.get('content')
-    model.start_at = request.POST.get('start_at')
-    model.end_at = request.POST.get('end_at')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись обновлена!"})
-
-
-@require_http_methods(["POST"])
-def report_create(request):
-    model = models.Report()
-
-    model.content = request.POST.get('content')
-    model.start_at = request.POST.get('start_at')
-    model.end_at = request.POST.get('end_at')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись создана!"})
-
-
-def parentteachermeeting(request):
-    objects = models.ParentTeacherMeeting.objects.all()
-    return render(request, "tables/curator_work/parentteachermeeting.html", {"content": objects})
-
-
-def parentteachermeeting_modal(request):
-    if request.method == "GET":
-        initial_id = request.GET.get("initial_id", '')
-        if initial_id != '':
-            # Если это запись для редактирования
-            initial_model = models.ParentTeacherMeeting.objects.get(id=initial_id)
-
-            initial_data = {
-                "content": initial_model.content,
-                "date": initial_model.date,
-            }
-            form = forms.ParentTeacherMeetingForm(initial=initial_data)
-            return render(request, "modals/curator_work/modalParentTeacherMeeting.html", {"form": form, 'initial_id': initial_id})
-        else:
-            form = forms.ParentTeacherMeetingForm()
-            return render(request, "modals/curator_work/modalParentTeacherMeeting.html", {"form": form, 'initial_id': ''})
-
-
-@require_http_methods(["GET"])
-def parentteachermeeting_delete(request):
-    models.ParentTeacherMeeting.objects.get(id=request.GET.get("id", '')).delete()
-    return JsonResponse({"result": True, "text": "Запись удалена!"})
-
-
-@require_http_methods(["POST"])
-def parentteachermeeting_edit(request, pk):
-    model = models.ParentTeacherMeeting.objects.get(id=pk)
-
-    model.content = request.POST.get('content')
-    model.date = request.POST.get('date')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись обновлена!"})
-
-
-@require_http_methods(["POST"])
-def parentteachermeeting_create(request):
-    model = models.ParentTeacherMeeting()
-
-    model.content = request.POST.get('content')
-    model.date = request.POST.get('date')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись создана!"})
-
-
-# educationalactivities
+@require_http_methods(["GET", "POST"])
 def educationalactivities(request):
-    objects = models.EducationalActivities.objects.all()
-    return render(request, "tables/curator_work/educationalactivities.html", {"content": objects})
+    def get_initial_data(_model):
+        initial_data = {
+            "activity": _model.activity,
+            "date": _model.date,
+            "note": _model.note,
+        }
+        return initial_data
+
+    def save_model(_model):
+        # Поля для изменения в каждой форме.
+        _model.activity = request.POST.get('activity')
+        _model.date = request.POST.get('date')
+        _model.note = request.POST.get('note')
+        _model.save()
+
+    model = models.EducationalActivities
+    form = forms.EducationalActivitiesForm
+    table_link = "tables/curator_work/educationalactivities.html"
+    return process_the_request(request, model, form, table_link, save_model, get_initial_data)
 
 
-def educationalactivities_modal(request):
-    if request.method == "GET":
-        initial_id = request.GET.get("initial_id", '')
-        if initial_id != '':
-            # Если это запись для редактирования
-            initial_model = models.EducationalActivities.objects.get(id=initial_id)
+@require_http_methods(["GET", "POST"])
+def parentteachermeeting(request):
+    def get_initial_data(_model):
+        initial_data = {
+            "content": _model.content,
+            "date": _model.date,
+        }
+        return initial_data
 
-            initial_data = {
-                "activity": initial_model.activity,
-                "date": initial_model.date,
-                "note": initial_model.note,
-            }
-            form = forms.EducationalActivitiesForm(initial=initial_data)
-            return render(request, "modals/curator_work/modalActivity.html", {"form": form, 'initial_id': initial_id})
-        else:
-            form = forms.EducationalActivitiesForm()
-            return render(request, "modals/curator_work/modalActivity.html", {"form": form, 'initial_id': ''})
+    def save_model(_model):
+        # Поля для изменения в каждой форме.
+        _model.content = request.POST.get('content')
+        _model.date = request.POST.get('date')
+        _model.save()
 
-
-@require_http_methods(["GET"])
-def educationalactivities_delete(request):
-    models.EducationalActivities.objects.get(id=request.GET.get("id", '')).delete()
-    return JsonResponse({"result": True, "text": "Запись удалена!"})
-
-
-@require_http_methods(["POST"])
-def educationalactivities_edit(request, pk):
-    model = models.EducationalActivities.objects.get(id=pk)
-
-    model.activity = request.POST.get('activity')
-    model.date = request.POST.get('date')
-    model.note = request.POST.get('note')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись обновлена!"})
-
-
-@require_http_methods(["POST"])
-def educationalactivities_create(request):
-    model = models.EducationalActivities()
-
-    model.activity = request.POST.get('activity')
-    model.date = request.POST.get('date')
-    model.note = request.POST.get('note')
-
-    model.save()
-    return JsonResponse({"result": True, "text": "Запись создана!"})
+    model = models.ParentTeacherMeeting
+    form = forms.ParentTeacherMeetingForm
+    table_link = "tables/curator_work/parentteachermeeting.html"
+    return process_the_request(request, model, form, table_link, save_model, get_initial_data)
