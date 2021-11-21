d2f312e99d81e46e6fa9d4dba3acda4d245ced2a
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-21 19:39:03 +0500

    url comment

--- homeroom/urls.py
+++ homeroom/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # class-period
     url('^class-period/$', views.class_period, name="class_period"),
+    # spent-class-period
     url('^spent-class-period/$', views.spent_class_period, name="spent_class_period"),
 ]
