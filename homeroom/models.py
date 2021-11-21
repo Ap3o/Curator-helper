45e78a7ca214d4b41f0ebdf3c3c11b99561e4599
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-21 17:34:50 +0500

    Изменил модель ClassPeriod

--- homeroom/models.py
+++ homeroom/models.py
@@ -4,7 +4,21 @@ from django.db import models
 # Тематика классных часов по месяцам
 class ClassPeriod(models.Model):
     topic = models.CharField(max_length=200, verbose_name='Тема собрания')
-    month = models.DateTimeField(verbose_name='Месяц')
+    MONTH_CHOICES = [
+        ('Январь', 'Январь'),
+        ('Февраль', 'Февраль'),
+        ('Март', 'Март'),
+        ('Апрель', 'Апрель'),
+        ('Май', 'Май'),
+        ('Июнь', 'Июнь'),
+        ('Июль', 'Июль'),
+        ('Август', 'Август'),
+        ('Сентябрь', 'Сентябрь'),
+        ('Октябрь', 'Октябрь'),
+        ('Ноябрь', 'Ноябрь'),
+        ('Декаберь', 'Декабрь')
+    ]
+    month = models.CharField(max_length=15, choices=MONTH_CHOICES, verbose_name='Месяц')
 
     class Meta:
         verbose_name_plural = "Тематика классных часов"
