8c3cafc2860c1ff7856c8f9aaf394ecc8d9cd778
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-22 23:48:44 +0500

    Попытка интегрировать диаграммы

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from education import models
+from django.db.models import Count
 
 
 # Create your views here.
@@ -7,4 +9,18 @@ def index(request):
 
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    objects = models.AcademicPerformance.objects.values('subject', 'mark').order_by('subject').annotate(
+        count=Count('mark')
+    )
+
+    data = dict()
+    subject_id = objects[0]['subject']
+    data[subject_id] = {"labels": [], "datasets": []}
+    for _object in objects:
+        if subject_id != _object['subject']:
+            break
+            subject_id = _object['subject']
+            data[subject_id] = dict()
+        data[subject_id]['labels'].append(str(_object['mark']))
+        data[subject_id]['datasets'].append(_object['count'])
+    return render(request, "dashboard.html", {"data": data})
