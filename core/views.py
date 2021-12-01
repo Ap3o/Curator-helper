57c4665038cf9879a0534757cf634b82a9efc2cd
Nikita Flesakov <flesakov.nik09@gmail.com>
2021-12-01 12:56:56 +0300

    404 500 pages

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from education import models
 from django.db.models import Count
+from django.template import RequestContext
 
 
 # Create your views here.
@@ -13,10 +14,10 @@ def get_subject_name(subject_id):
 
 
 def final_performance(request):
-    objects = models.AcademicPerformance.objects\
-        .values('subject', 'mark')\
-        .filter(type_of_perfomance="Итоговая")\
-        .order_by('subject')\
+    objects = models.AcademicPerformance.objects \
+        .values('subject', 'mark') \
+        .filter(type_of_perfomance="Итоговая") \
+        .order_by('subject') \
         .annotate(count=Count('mark'))
 
     data = dict()
@@ -31,3 +32,15 @@ def final_performance(request):
         data[subject_name]['labels'].append(str(_object['mark']))
         data[subject_name]['datasets'].append(_object['count'])
     return render(request, "dashboard.html", {"data": data})
+
+
+def handler404(request, exception, template_name="page404.html"):
+    response = render(request, template_name)
+    response.status_code = 404
+    return response
+
+
+def handler500(request, exception, template_name="page500.html"):
+    response = render(request, template_name)
+    response.status_code = 500
+    return response
--- kursovoy_4kurs/urls.py
+++ kursovoy_4kurs/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'core.views.handler404'
+handler500 = 'core.views.handler500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('education/', include('education.urls')),
